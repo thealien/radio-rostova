5fd3347c657a88cde9c091681615980620f759ea
thealien <roman.ziva@gmail.com>
2013-01-24 19:19:26 +0400

    remove dev libs from git

--- libs/tracker/checker/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-exports.create = function (config, modules) {
-    if (!modules[config.type]) {
-        throw new Error('No checker module for ' + config.type);
-    }
-    return modules[config.type].create(config.config);
-};
--- libs/tracker/updater/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.create = function (config, modules) {
-    if (!modules[config.type]) {
-        throw new Error('No updater module for ' + config.type);
-    }
-    return modules[config.type].create(config.config);
-};
